f9ca13c756858954409bb81dd32d20f1548bd290
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 19:40:26 +0600

    feat: add navlink common component for showing active navlink

---
+++ src/components/common/navlink.tsx
@@ -0,0 +1,26 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+
+export interface NavLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+    href: string;
+    children: ReactNode;
+    active: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
+    other: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
+}
+
+const NavLink = ({ href, children, active, other, className, ...props }: NavLinkProps) => {
+    const pathname = usePathname();
+    return (
+        <Link href={href} {...props}>
+            <Button className={cn("md:h-10 xl:h-11 w-full font-semibold capitalize justify-start px-3 md:px-3", className)} variant={pathname === href ? active : other}>
+                {children}
+            </Button>
+        </Link>
+    );
+};
+
+export default NavLink;
