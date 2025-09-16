c8b0f0494fed2141432e393dab8f765b8b6cb068
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 19:42:07 +0600

    feat: add header with quick create button

--- src/components/layout/dashboard/header/header.tsx
+++ src/components/layout/dashboard/header/header.tsx
@@ -1,9 +1,19 @@
+import Icon from '@/components/common/icon';
+import { Button } from '@/components/ui/button';
+import { PlusSignCircleIcon } from '@hugeicons/core-free-icons';
 import { FC } from 'react';
 
 const DashboardHeader: FC = () => {
     return (
-        <div>
-            DashboardHeader
+        <div className='flex items-center justify-between px-6 py-4 bg-white border-b max-h-16'>
+            <h1 className='text-xl font-semibold text-gray-900'>Documents</h1>
+            <Button className='bg-emerald-500 hover:bg-emerald-600 text-white'>
+                <Icon
+                    icon={PlusSignCircleIcon}
+                    size={16}
+                />
+                <span>Quick Create</span>
+            </Button>
         </div>
     );
 };
