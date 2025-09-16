e568d21e9d181202e5fcb591799ac14fb32a8ea1
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 16:07:27 +0600

    feat: add placeholder layout component on root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,19 @@
+import Footer from '@/components/layout/root/footer/footer';
+import Navbar from '@/components/layout/root/navbar/navbar';
 import { FC, ReactNode } from 'react';
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <>
+            <header>
+                <Navbar />
+            </header>
             <main>
                 {children}
             </main>
+            <footer>
+                <Footer />
+            </footer>
         </>
     );
 };
