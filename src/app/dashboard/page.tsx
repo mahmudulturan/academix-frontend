0c2f38b82e4d8f87df81f4cb6d44bb7edcfb9cfd
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 15:59:57 +0600

    refactor: dashboard home page path

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const DashboardPage: FC = () => {
+    return (
+        <div>
+            Dashboard Page
+        </div>
+    );
+};
+
+export default DashboardPage;
