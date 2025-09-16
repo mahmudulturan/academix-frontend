cd4ae7c371c2777fa74b59152df8c8dc5e8fa523
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 20:33:31 +0600

    feat: add Breadcrumb  on dashboard header

--- src/components/layout/dashboard/header/header.tsx
+++ src/components/layout/dashboard/header/header.tsx
@@ -1,13 +1,72 @@
+"use client";
 import Icon from '@/components/common/icon';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
-import { PlusSignCircleIcon } from '@hugeicons/core-free-icons';
+import { Separator } from '@/components/ui/separator';
+import { PlusSignCircleIcon, SidebarLeft01Icon, SidebarRight01Icon } from '@hugeicons/core-free-icons';
+import { usePathname } from 'next/navigation';
 import { FC } from 'react';
 
-const DashboardHeader: FC = () => {
+interface DashboardHeaderProps {
+    isSidebarExpanded: boolean;
+    setIsSidebarExpanded: (isSidebarExpanded: boolean) => void;
+}
+
+const DashboardHeader: FC<DashboardHeaderProps> = ({ isSidebarExpanded, setIsSidebarExpanded }) => {
+    const pathname = usePathname();
+
+    const generateBreadcrumbs = () => {
+        const paths = pathname.split('/').filter(path => path).slice(pathname.split('/').length - 3, pathname.split('/').length);
+        return paths.map((path, index) => {
+            const href = `/${paths.slice(0, index + 1).join('/')}`;
+            const label = path.charAt(0).toUpperCase() + path.slice(1);
+            const isLast = index === paths.length - 1;
+
+            return (
+                <BreadcrumbList key={index}>
+                    <BreadcrumbItem>
+                        {isLast ? (
+                            <BreadcrumbPage className='font-semibold'>{label}</BreadcrumbPage>
+                        ) : (
+                            <BreadcrumbLink className='font-semibold' href={href}>{label}</BreadcrumbLink>
+                        )}
+                    </BreadcrumbItem>
+                    {!isLast && <BreadcrumbSeparator />}
+                </BreadcrumbList>
+            );
+        });
+    };
+
+    const toggleSidebar = () => setIsSidebarExpanded(!isSidebarExpanded);
+
     return (
-        <div className='flex items-center justify-between px-6 py-4 bg-white border-b max-h-16'>
-            <h1 className='text-xl font-semibold text-gray-900'>Documents</h1>
-            <Button>
+        <div className='flex items-center justify-between px-6 py-4 border-b max-h-16'>
+            <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-0.5'>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={toggleSidebar}
+                    >
+                        {isSidebarExpanded ? (
+                            <Icon
+                                icon={SidebarLeft01Icon}
+                            />
+                        ) : (
+                            <Icon
+                                icon={SidebarRight01Icon}
+                            />
+                        )}
+                        <span className="sr-only">Toggle Sidebar</span>
+                    </Button>
+                    <Separator orientation='vertical' className='h-5' />
+                </div>
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        {generateBreadcrumbs()}
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>            <Button>
                 <Icon
                     icon={PlusSignCircleIcon}
                     size={16}
