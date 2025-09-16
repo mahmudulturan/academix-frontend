a01ada2f6071c392d9b7736eb5b57d89259e50ec
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-16 23:39:43 +0600

    feat: add loading state to Button component with spinner icon

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,8 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import Icon from "../common/icon"
+import { Loading01Icon } from "@hugeicons/core-free-icons"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
@@ -40,19 +42,27 @@ function Button({
   variant,
   size,
   asChild = false,
+  loading = false,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
+    loading?: boolean;
   }) {
   const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
       data-slot="button"
+      disabled={props.disabled || loading}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      <>
+        {props.children}
+        {loading && <Icon icon={Loading01Icon} className="animate-spin" />}
+      </>
+    </Comp>
   )
 }
 
