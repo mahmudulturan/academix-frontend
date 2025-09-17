58d07af028d2bfce135a61997e4a2223cb9ad30e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-17 06:20:56 +0600

    refactor: routes for localization

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+
 import ThemeProvider from '@/providers/theme-provider';
 import TanstackProvider from '@/providers/tanstack-provider';
 import { Toaster } from 'sonner';
