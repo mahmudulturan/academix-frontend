8c090632f55c4dc208dc0a9fbebc47bcbd03a18e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-17 08:20:15 +0600

    feat: add success message for login in Bengali and English translations

--- src/tanstack/mutation/auth.mutation.ts
+++ src/tanstack/mutation/auth.mutation.ts
@@ -1,3 +1,5 @@
+import { useRouter } from "@/i18n/routing";
+import { useErrorTranslations, useSuccessTranslations } from "@/lib/i18n-utils";
 import { authServices } from "@/services/auth.services";
 import { useStore } from "@/store";
 import { ILoginPayload } from "@/types/auth.type";
@@ -7,15 +9,18 @@ import { toast } from "sonner";
 
 export const useLoginUserMutation = () => {
     const { setUser } = useStore(state => state.auth);
-
+    const successT = useSuccessTranslations();
+    const errorT = useErrorTranslations();
+    const router = useRouter();
     return useMutation({
         mutationFn: async (data: ILoginPayload) => await authServices.loginUser(data),
         onSuccess: (data) => {
             setUser(data.data);
-            toast.success(data?.message || "Login Successfully");
+            toast.success(successT('loginSuccessfully'));
+            router.push("/dashboard");
         },
-        onError: (error : IErrorResponse) => {
-            toast.error(error?.response?.data?.message || "Something went wrong");
+        onError: (error: IErrorResponse) => {
+            toast.error(errorT("somethingWentWrong"));
         }
     })
 }
