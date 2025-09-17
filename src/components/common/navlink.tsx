59865078828c11a550b507a09973d91fae3fcba7
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-17 06:52:07 +0600

    refactor: update NavLink component to use localized Link and usePathname from routing

--- src/components/common/navlink.tsx
+++ src/components/common/navlink.tsx
@@ -1,9 +1,8 @@
 "use client"
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
+import { Link, usePathname } from "@/i18n/routing";
 
 export interface NavLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
     href: string;
