5b8be8903e124f5c2d3597d2c356df1e25d55761
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-18 06:56:26 +0600

    feat: add auth type interface

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -3,3 +3,33 @@ import { z } from "zod";
 
 const loginSchema = createLoginSchema((key) => key);
 export type ILoginPayload = z.infer<typeof loginSchema>;
+
+export interface IRegisterPayload {
+    name: {
+        en: string;
+        bn: string;
+    };
+    email: string;
+    phone: string;
+    password: string;
+    profileImage?: string;
+}
+
+export interface IChangePasswordPayload {
+    oldPassword: string;
+    newPassword: string;
+}
+
+export interface IVerifyEmailPayload {
+    email: string;
+    verificationCode: string;
+}
+
+export interface IVerifyPasswordResetOtpPayload {
+    otp: string;
+    email: string;
+}
+
+export interface IResetPasswordPayload {
+    newPassword: string;
+}
