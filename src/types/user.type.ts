b7a33cb2e9051ac4e7926e11b39ae943d72da3a6
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-18 07:24:56 +0600

    feat: add user types with get all user params and mutation payload

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -6,17 +6,38 @@ export interface IName {
 }
 
 export interface IUser {
+    _id: string;
     id: string;
     name: IName;
     email: string;
     phone: string;
-    username: string;
+    username?: string;
     profileImage?: string;
     roles: string[];
     status: typeof USER_STATUS[keyof typeof USER_STATUS];
     isEmailVerified: boolean;
-    isDeleted: boolean;
+    isDeleted?: boolean;
     permissions: string[];
     createdAt?: Date;
     updatedAt?: Date;
+}
+
+export interface IGetAllUsersParams {
+    page?: number;
+    limit?: number;
+    searchKey?: string;
+    sort?: string;
+    status?: "active" | "blocked";
+    populate?: string;
+    fields?: string;
+    rolesFields?: string;
+}
+
+export interface IUpdateMyProfilePayload {
+    name: IName;
+    profileImage?: string;
+}
+
+export interface IUpdateUserStatusPayload {
+    status: "active" | "blocked";
 }
