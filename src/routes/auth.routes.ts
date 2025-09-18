68c4160129007dfda6e06eb006de6eede654de20
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-09-18 06:56:02 +0600

    feat: add all auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,15 @@
 const authRoutes = {
+    register: "/auth/register",
     login: "/auth/login",
-    getCurrentUser: "/auth/me"
+    getCurrentUser: "/auth/me",
+    changePassword: "/auth/password/change",
+    sendVerificationEmail: (email: string) => `/auth/email/verification/${email}`,
+    verifyEmail: "/auth/email/verify-email",
+    sendPasswordResetEmail: (email: string) => `/auth/password/reset-email/${email}`,
+    verifyPasswordResetOtp: "/auth/password/reset/verify-otp",
+    resetPassword: (token: string) => `/auth/password/reset/${token}`,
+    refreshToken: "/auth/token/refresh",
+    logout: "/auth/logout"
 }
 
 export default authRoutes;
